71761e96a44f05fbe0b8571ac7b09abbda4d8a04
Matt Chun-Lum <matt@questionable.engineering>
2024-12-22 15:30:37 -0800

    perf(day022): reduce encoding max size

--- day-022-monkey-market/src/lib.rs
+++ day-022-monkey-market/src/lib.rs
@@ -8,9 +8,40 @@ use nom::{
 };
 use rayon::prelude::*;
 
+// -9 Ob00000  0
+// -8 Ob00001  1
+// -7 Ob00010  2
+// -6 Ob00011  3
+// -5 Ob00100  4
+// -4 Ob00101  5
+// -3 Ob00110  6
+// -2 Ob00111  7
+// -1 Ob01000  8
+//  0 Ob01001  9
+//  1 Ob01010  10
+//  2 Ob01011  11
+//  3 Ob01100  12
+//  4 Ob01101  13
+//  5 Ob01110  14
+//  6 Ob01111  15
+//  7 Ob10000  16
+//  8 Ob10001  17
+//  9 Ob10010  18
+//
+//  so, really, we only need a few numbers beyond
+//  so the biggest number that only needs 19 bits would be
+//                          6    3    0    0
+// const SM_SEQ_MAX: usize = 0b01111011000100101001;
+
 // N % 16,777,216 is equal to N & MOD_MASK;
 const MOD_MASK: u64 = (1 << 24) - 1;
 const SEQ_MASK: usize = (1 << 20) - 1;
+// Under our encoding scheme, the max value is the following sequence
+//                       9     0     0     0
+const SEQ_MAX: usize = 0b10010_01001_01001_01001;
+// and the minimum is   -9     0     0     0
+const SEQ_MIN: usize = 0b00000_01001_01001_01001;
+const SEQ_SIZE: usize = SEQ_MAX + 1 - SEQ_MIN;
 const DESIRED_CHUNKS: usize = 4;
 
 #[derive(Debug, Clone)]
@@ -34,8 +65,8 @@ impl FromStr for MonkeyMarket {
         let (p1, p2, _totals) = initial_numbers
             .par_chunks(chunk_size)
             .map(|chunk| {
-                let mut totals = vec![0_u16; 1 << 20];
-                let mut seen = vec![usize::MAX; 1 << 20];
+                let mut totals = vec![0_u16; SEQ_SIZE];
+                let mut seen = vec![usize::MAX; SEQ_SIZE];
                 let mut num_total = 0;
                 let mut best = 0;
 
@@ -49,12 +80,14 @@ impl FromStr for MonkeyMarket {
                         let cur_digit = (cur % 10) as i8;
                         let delta: i8 = cur_digit - prev;
                         prev = cur_digit;
-                        key = ((key << 5) & SEQ_MASK) | (delta + 10) as usize;
+                        key = ((key << 5) & SEQ_MASK) | (delta + 9) as usize;
 
-                        if j > 2 && seen[key] != i {
-                            seen[key] = i;
-                            totals[key] += cur_digit as u16;
-                            best = best.max(totals[key])
+                        let adjusted_key = key - SEQ_MIN;
+
+                        if j > 2 && seen[adjusted_key] != i {
+                            seen[adjusted_key] = i;
+                            totals[adjusted_key] += cur_digit as u16;
+                            best = best.max(totals[adjusted_key])
                         }
                     }
                     num_total += cur;
@@ -63,7 +96,7 @@ impl FromStr for MonkeyMarket {
                 (num_total, best, totals)
             })
             .reduce(
-                || (0_u64, 0_u16, vec![0_u16; 1 << 20]),
+                || (0_u64, 0_u16, vec![0_u16; SEQ_SIZE]),
                 |(mut total_num, mut best, mut acc),
                  (chunk_total_num, chunk_best, chunk_totals)| {
                     total_num += chunk_total_num;
